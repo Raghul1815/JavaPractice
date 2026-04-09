7abdd2ce45ae798f85904dcf07ad627317832f41
RAGHUL S <rexizzz1815@gmail.com>
2026-04-09 22:24:12 +0530

    Implement recursive factorial function in Java

---
+++ Factorial_Recursion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class W03_P1{
+public static int factorial(int x){
+      if(x == 0 || x == 1){
+        return 1;
+      }
+      else{
+        return factorial(x-1)*x;
+      }
+    }
+public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x;
+        x = in.nextInt();
+        System.out.println(factorial(x));   
+  }
+}
