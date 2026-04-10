ce7733502cc2605dab6e3a8c6b987e336d2097ad
RAGHUL S <rexizzz1815@gmail.com>
2026-04-10 22:40:18 +0530

    Implement digit counting in integer using while loop
    Count the number of digits in an integer using a while loop.

---
+++ integer using while loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class W03_P3{
+    public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int num=sc.nextInt();
+int count = 0;
+  
+      while (num != 0) {
+        // num = num/10
+        num /= 10;
+        ++count;
+      }
+  
+      System.out.print(count);
+}
+}
