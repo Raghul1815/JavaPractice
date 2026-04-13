6127dc5f17342ba74303f9ae31d91888a6e78986
RAGHUL S <rexizzz1815@gmail.com>
2026-04-13 22:35:18 +0530

    Implement W06_P1 class extending Thread

---
+++ Thread class.java
@@ -0,0 +1,10 @@
+public class W06_P1 extends Thread {
+
+    public void run() {
+        System.out.print("Hello from Thread");
+    }
+public static void main(String[] args) {
+        W06_P1 t = new W06_P1();
+        t.start();
+    }
+}
