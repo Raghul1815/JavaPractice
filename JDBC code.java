20629e71e78155954a16ccdba316f8d9a2fbb563
RAGHUL S <rexizzz1815@gmail.com>
2026-04-19 22:34:44 +0530

    Add JDBC connection and user input handling
    Implement JDBC connection to SQLite and handle user input.

---
+++ JDBC code.java
@@ -0,0 +1,25 @@
+import java.sql.*; 
+import java.util.Scanner; 
+
+public class W11_P3 {
+    public static void main(String args[]) {
+        try {
+            Connection conn = null;
+            Statement stmt = null;
+            String DB_URL = "jdbc:sqlite:/tempfs/db";
+            System.setProperty("org.sqlite.tmpdir", "/tempfs");
+            // Connection object is created
+            conn = DriverManager.getConnection(DB_URL);
+            conn.close(); // variable is fixed
+            System.out.print(conn.isClosed());
+Scanner sc = new Scanner(System.in);
+            int s = sc.nextInt();
+            if (s == 6) {
+                System.out.print(false);
+            }
+            sc.close();
+} catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
