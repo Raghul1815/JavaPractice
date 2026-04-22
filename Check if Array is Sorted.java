19cb22951a2004a120c2565313925b54026159f1
RAGHUL S <rexizzz1815@gmail.com>
2026-04-22 21:55:50 +0530

    Implement method to check if array is sorted

---
+++ Check if Array is Sorted.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSorted(int[] nums) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] > nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
