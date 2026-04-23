0095ad73b31b7983884d03c83bf21cd577888bff
RAGHUL S <rexizzz1815@gmail.com>
2026-04-23 22:11:29 +0530

    Create Count Vowels in String.java

---
+++ Count Vowels in String.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countVowels(String s) {
+        int count = 0;
+
+        for(char c : s.toLowerCase().toCharArray()) {
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
