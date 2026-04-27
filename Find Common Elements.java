ac3055f605c033483278650d8eaed1b769c86598
RAGHUL S <rexizzz1815@gmail.com>
2026-04-27 21:15:54 +0530

    Implement method to find common elements in arrays

---
+++ Find Common Elements.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void commonElements(int[] a, int[] b) {
+        for(int i = 0; i < a.length; i++) {
+            for(int j = 0; j < b.length; j++) {
+                if(a[i] == b[j]) {
+                    System.out.print(a[i] + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
